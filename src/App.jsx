252104ee3154f3c43baf0fd81bbbfd0313905562
fhsolutions2025 <teams@fullhousey.com>
2025-08-16 08:04:55 +0530

    summary
    modified

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>FullHousey Admin</h1>
+      <p>Dashboard loading...</p>
+    </div>
+  );
+}
+
+export default App;
